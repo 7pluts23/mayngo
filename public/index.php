24436c1c50052aaffde7137330d19cb4e77227c8
Stephen Pluta <stephen.pluta@gmail.com>
2013-09-22 05:08:03 +0000

    Finished stream 2

--- public/index.php
+++ public/index.php
@@ -1,4 +1,13 @@
 <?php
 //	Controller
-echo 'testing';
-?>
+	
+	if($_SERVER['REQUEST_URI'] != '/') {
+		//Homepage
+		preg_match('!name/([a-z]+)!imsx', $_SERVER['REQUEST_URI'], $pmatches);
+
+		$content = file_get_contents("../templates/index.html");
+		$content = str_replace('{text}', 'Hello, '.$pmatches[1], $content); 
+		echo $content;
+	} else {
+		//Not homepage
+	}
